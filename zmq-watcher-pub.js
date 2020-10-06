f87e69ed75e3eb32dfb7c5b063f8b3602d1c4170
Abraham E. Tavarez <abetavarez@icloud.com>
2020-10-06 19:22:22 -0400

    second file tested

--- zmq-watcher-pub.js
+++ zmq-watcher-pub.js
@@ -1,14 +1,19 @@
 `use strict`;
 
+//* Publisher /////////////////////
+
 const fs = require(`fs`);
 const zmq = require(`zeromq`);
 const filename = process.argv[2];
+
+// Other files
+const secTarget = __dirname + `/secTarget.txt`;
 
 // Create the pub/publisher endpoint.
 const publisher = zmq.socket('pub');
 
 // fs.watch is only called once
-fs.watch(filename, () => {
+fs.watch(secTarget, () => {
   // Send a message to any and all subscribers.
   publisher.send(
     JSON.stringify({
