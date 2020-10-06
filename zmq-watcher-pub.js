3ffb77cf997a93d3a54235ab6b9b3cec6ffbb7e2
Abraham E. Tavarez <abetavarez@icloud.com>
2020-10-06 18:50:35 -0400

    running good

--- zmq-watcher-pub.js
+++ zmq-watcher-pub.js
@@ -1,23 +1,28 @@
-`use strict`
+`use strict`;
 
-import fs from `fs`;
-import zmq from `zeromq`;
-import filename from process.argv[2];
+const fs = require(`fs`);
+const zmq = require(`zeromq`);
+const filename = process.argv[2];
 
 // Create the pub/publisher endpoint.
-const publisher = zmq.Socket(`pub`);
+const publisher = zmq.socket('pub');
 
+// fs.watch is only called once
 fs.watch(filename, () => {
   // Send a message to any and all subscribers.
-  publisher.send(JSON.stringify({
-    type: `changed`,
-    file: filename,
-    timestamp: Date.now()
-  }));
+  publisher.send(
+    JSON.stringify({
+      type: `changed`,
+      file: filename,
+      timestamp: Date.now(),
+    })
+  );
 });
 
 // Listen on TCP port 60400
-publisher.bind(`tcp://*:60400`, err => {
-  if (err) throw err;
-  console.log(`Listening for zmq subscribers...`)
+publisher.bind(`tcp://*:60400`, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening for zmq subscribers...`);
 });
